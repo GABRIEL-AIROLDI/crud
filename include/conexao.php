a0db9cf71da9e62bada041d36c9023a64acd04a4
Manuel Gonzalez <87993163072@senacrs.edu.br>
2025-09-01 10:01:48 -0300

    v01

---
+++ include/conexao.php
@@ -0,0 +1,10 @@
+<?php
+
+define( "DBSERVER","localhost");
+define("DBUSER","root");
+define("DBPASS","");
+define("DBBASE","empresa");
+
+$conexao = mysqli_connect(DBSERVER,DBUSER,DBPASS,DBBASE);
+
+?>
--- lista-cargos.php
+++ lista-cargos.php
@@ -1,12 +1,12 @@
-<?php 
+<?php
 // include dos arquivox
 include_once './include/logado.php';
 include_once './include/conexao.php';
 include_once './include/header.php';
-
+ 
 ?>
   <main>
-
+ 
     <div class="container">
         <h1>Lista de Cargos</h1>
         <a href="./salvar-cargos.php" class="btn btn-add">Incluir</a>
@@ -20,31 +20,31 @@ include_once './include/header.php';
             </tr>
           </thead>
           <tbody>
+            <?php
+            $sql = 'SELECT * FROM cargos;';
+            $resultado = mysqli_query($conexao,$sql);
+ 
+            while ( $dado = mysqli_fetch_assoc($resultado)) {
+            ?>
             <tr>
               <td>1</td>
-              <td>Dado A</td>
-              <td>100</td>
+              <td><?php echo $dado['Nome'];?></td>
+              <td>R$ <?php echo $dado['TetoSalarial']?></td>
               <td>
                 <a href="salvar-cargos.php?id=" class="btn btn-edit">Editar</a>
                 <a href="#" class="btn btn-delete">Excluir</a>
               </td>
             </tr>
-            <tr>
-              <td>2</td>
-              <td>Dado B</td>
-              <td>250</td>
-              <td>
-                <a href="salvar-cargos.php?id=" class="btn btn-edit">Editar</a>
-                <a href="#" class="btn btn-delete">Excluir</a>
-              </td>
-            </tr>
-            
+            <?php
+            }
+            ?>
+           
           </tbody>
         </table>
-      </div> 
+      </div>
   </main>
-  
-  <?php 
+ 
+  <?php
   // include dos arquivox
   include_once './include/footer.php';
   ?>
