351e1d6a151e53d1866d18dcb4b2d7aeaec4241a
Manuel Gonzalez <87993163072@senacrs.edu.br>
2025-09-25 08:10:11 -0300

    fase3

--- salvar-cargos.php
+++ salvar-cargos.php
@@ -1,27 +1,39 @@
-<?php 
+<?php
 // include dos arquivox
 include_once './include/logado.php';
 include_once './include/conexao.php';
 include_once './include/header.php';
+ 
+//pega o id
+$id = $_GET['id'];
+ 
+//montar o sql
+$sql = "SELECT * FROM cargos WHERE CargoID = $id;";
+ 
+//executar o sql
+$resultado = mysqli_query($conexao,$sql);
+ 
+//pega o dado
+$row = mysqli_fetch_assoc($resultado);
 ?>
   <main>
-
+ 
        <!-- Telas CRUD -->
    <div id="cargos" class="tela">
     <form class="crud-form" action="./action/cargos.php" method="post">
       <h2>Cadastro de Cargos</h2>
-      <input type="text" placeholder="Nome do Cargo">
-      <input type="number" placeholder="Teto Salarial">
+      <input type="text" placeholder="Nome do Cargo" value="<?php echo $row['Nome'];?>">
+      <input type="number" placeholder="Teto Salarial" value="<?php echo $row['TetoSalarial'];?>">
       <button type="submit">Salvar</button>
     </form>
   </div>
-
-
-
+ 
+ 
+ 
    
   </main>
-
-  <?php 
-  // include dos arquivox
+ 
+  <?php
+ 
   include_once './include/footer.php';
   ?>
